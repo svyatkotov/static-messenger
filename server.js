8832b4a46631d49b3a8ebda3eb822946a862650f
svyatkotov <skd946@gmail.com>
2020-06-15 13:29:46 +0300

    server.js fix after code review

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
+const page = 'chat';
+
 app.use(express.static('./static'));
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порте ${PORT}!`);
+  console.log(`Страница доступна по адресу: http://localhost:${PORT}/${page}.html`);
 });
